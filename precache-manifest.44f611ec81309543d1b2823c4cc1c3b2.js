49630241f1d393f2cad28b61c491f520f330389f
Atulya Raj <atulya.raj2904@gmail.com>
2019-06-11 13:00:16 +0530

    Updates

---
+++ precache-manifest.44f611ec81309543d1b2823c4cc1c3b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa7307916958eb01a67efe92ceffdf9",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "76c61bf36b1c1162c01e",
+    "url": "/Tic-Tac-Toe/static/css/main.181605d9.chunk.css"
+  },
+  {
+    "revision": "ffbe32d4e790e6a5d2f2",
+    "url": "/Tic-Tac-Toe/static/js/2.335f0c66.chunk.js"
+  },
+  {
+    "revision": "76c61bf36b1c1162c01e",
+    "url": "/Tic-Tac-Toe/static/js/main.b7de6e11.chunk.js"
+  },
+  {
+    "revision": "10779627d94960e7b271",
+    "url": "/Tic-Tac-Toe/static/js/runtime~main.adeb2bb3.js"
+  }
+]);
